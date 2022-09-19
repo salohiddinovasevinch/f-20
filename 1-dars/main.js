55d1ab63ee6d74014095601b4f31a5721117694c
Sevinch Salohiddinova <salohiddinova05@gmail.com>
2022-09-19 09:47:17 +0500

    Kirish darsi

---
+++ 1-dars/main.js
@@ -0,0 +1,15 @@
+// var name = "Sevinch"
+// console.log(name);
+
+
+var firstName = prompt("Ismingiz nima?")
+var age = prompt("Yoshingizni kiriting");
+
+console.log('Kirish darsi mavzusi' + ' ' + 'Javascript')
+
+console.log('Foydalanuvchining ismi  ' + firstName + '. va uning yoshi: '  + age)
+// console.log(firstName) 
+
+// console.log("G'oz")
+// console.log('G\'oz')
+
